8e20e100b4b174a45d57f58293199b76ed3e2aa9
Swetha <Snega>
2026-06-29 19:54:52 +0530

    initial commit

---
+++ app/app.py
@@ -0,0 +1,51 @@
+from flask import Flask
+import threading
+import time
+
+app = Flask(__name__)
+
+memory_hog = []
+
+@app.route("/")
+def home():
+    return {
+        "project": "AI-Assisted Self-Healing Kubernetes Platform",
+        "status": "running"
+    }
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+@app.route("/cpu-load")
+def cpu_load():
+
+    def burn_cpu():
+        end_time = time.time() + 60
+
+        while time.time() < end_time:
+            x = 0
+            for i in range(100000):
+                x += i * i
+
+    threading.Thread(target=burn_cpu).start()
+
+    return {
+        "message": "CPU load started for 60 seconds"
+    }
+
+@app.route("/memory-load")
+def memory_load():
+
+    global memory_hog
+
+    memory_hog.append("A" * 10_000_000)
+
+    return {
+        "message": "Memory allocation increased"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
